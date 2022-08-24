08e9878258bae529ed7efe9febc77b5fc2e2d1bf
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-24 20:51:32 +0300

    added tasks 34, 35, 36, 38

---
+++ Task35/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задайте одномерный массив из 123 случайных чисел. Найдите количество 
+// элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] array = new int[123];
+int count=0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j] > 9 & array[j] < 100) count = count + 1;
+}
+
+Console.WriteLine($"На отрезке [10,99] содержится - {count} элементов ");
---
+++ Task38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте с клавиатуры массив вещественных (double) чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+Console.WriteLine("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("массив:  ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    if (numbers[j] > max)
+    {
+        max = numbers[j];
+    }
+    if (numbers[j] < min)
+    {
+        min = numbers[j];
+    }
+}
+
+Console.WriteLine($"всего {numbers.Length} элементов. Max = {max}, Min = {min}");
+Console.WriteLine($"Разница между Max и Min  = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
